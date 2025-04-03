11eeca84b42d7b41ee439c9d841a645e65ee88f4
nekoprojectteam <dddm8122@gmail.com>
2025-04-03 21:10:07 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+const addonData = {
+    "dead_zone_original": {
+        title: "Dead Zone: Original",
+        image: "addon_thumbnail/original.png",
+        description: "DeadZone is a survival addon inspired by the game DayZ that puts you in the role of a survivor in a post-apocalyptic world. The goal is simple: survive as long as possible, search every building for loot, and craft your own story along the way.",
+        changelog: [
+            "Version 1.6.6-1",
+            "- Added Addon Menu setting (Items) - Cannot be removed",
+            "- Nerf M79",
+            "- Nerf Infected militia speed",
+            "- Corpse despawn when out of render range",
+            "- Updated some entities, items, and spawn rules",
+            "- Updated some textures",
+            "- Support for versions 1.21.70+",
+            "- Improved human animation",
+            "- Improved stability",
+            "- Updated entity loot_table rate chances",
+            "- Improved wind sounds",
+            "- Revamped some blocks' models",
+            "- Fixed bleeding system",
+            "- Fixed collect water giving you more water bottles",
+            "- Fixed vehicle sitting position",
+            "- Fixed player dying when generating new world",
+            "- Fixed menu music always playing (it should play only on loading screen)"
+        ],
+        versions: ["1.21.50", "1.21.60", "1.21.70", "1.21.80"],
+        tags: ["minecraft", "apocalyptic", "addon", "zombie"],
+        downloads: [
+            { label: "Lootdest", link: "https://lootdest.org/s?qFPxN0wW"
+            }
+        ],
+        releaseDate: "May 31, 2025",
+        updateDate: "April 3, 2025"
+    }
+};
