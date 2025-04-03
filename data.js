e1e3977450bec098f3f51b8e6aa57ca8f0dc377a
nekoprojectteam <dddm8122@gmail.com>
2025-04-04 00:51:32 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -5,28 +5,19 @@ const addonData = {
         description: "DeadZone is a survival addon inspired by the game DayZ that puts you in the role of a survivor in a post-apocalyptic world. The goal is simple: survive as long as possible, search every building for loot, and craft your own story along the way.",
         changelog: [
             "Version 1.6.6-1",
-            "- Added Addon Menu setting (Items) - Cannot be removed",
-            "- Nerf M79",
-            "- Nerf Infected militia speed",
-            "- Corpse despawn when out of render range",
-            "- Updated some entities, items, and spawn rules",
-            "- Updated some textures",
-            "- Support for versions 1.21.70+",
-            "- Improved human animation",
-            "- Improved stability",
-            "- Updated entity loot_table rate chances",
-            "- Improved wind sounds",
-            "- Revamped some blocks' models",
-            "- Fixed bleeding system",
-            "- Fixed collect water giving you more water bottles",
-            "- Fixed vehicle sitting position",
-            "- Fixed player dying when generating new world",
-            "- Fixed menu music always playing (it should play only on loading screen)"
+            "- Added spawn protection on players (10 seconds)",
+            "- Fixed player can shoot while running (they not supposed to shoot while running)",
+            "- Fixed Player die while making new world",
+            "- Fixed survivors not hold any guns",
+            "- Fixed menu item can be dropped when dies",
+            "Bug that exist",
+            "- Blood particles still come out, even though you have healed it",
+            "- City structure has "Unknown" texture"
         ],
         versions: ["1.21.50", "1.21.60", "1.21.70", "1.21.80"],
         tags: ["minecraft", "apocalyptic", "addon", "zombie"],
         downloads: [
-            { label: "Lootdest", link: "https://lootdest.org/s?qFPxN0wW"
+            { label: "Lootdest", link: "https://lootdest.org/s?FMlman2b"
             }
         ],
         releaseDate: "May 31, 2025",
